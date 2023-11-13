c788e1f1038f2a9984bf427f0f444da976b417cd
Jax Duan <j_duan168326@fanshaweonline.ca>
2023-11-12 23:37:00 -0500

    neweset update
    new version

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 ( () =>{
-    document.getElementById('hamburger-icon').addEventListener('click', function() {
-        // Toggle the visibility of the navigation menu
-        var nav = document.querySelector('.main-nav');
-        nav.style.display = (nav.style.display === 'block' ? 'none' : 'block');
-    });
-
-    
     //exopre images
     const canvas = document.querySelector("#explode-view")
     const context = canvas.getContext("2d");
@@ -65,8 +58,7 @@
         dragging = false,
         min = 0,
         max = imageCon.offsetWidth;
-        //The HTMLElement.offsetWidth read-only property returns the layout width of an element. 
-    
+
     function onDown() {
       dragging = true;
     }
@@ -78,17 +70,14 @@
     function onMove(event) {
       if(dragging===true) {
         var x = event.clientX - imageCon.getBoundingClientRect().left;
-        //The MouseEvent.clientX read-only property provides the horizontal coordinate within the application's client area at which the event occurred
-        //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-        //X-coordinate, relative to the viewport origin, of the left of the rectangle box. Read only
         console.log(event.clientX);
         console.log(imageCon.getBoundingClientRect().left);
-      //need logic to keep slider in box
-        if(x < min) { //if x less than 0
-          x = min;    //set x = 0
+  
+        if(x < min) { 
+          x = min;    
         }
-       else if(x > max) { //otherwise if x is greater than 900
-          x = max-4; //set x to equal the max width minus 2 (width of slider)
+       else if(x > max) { 
+          x = max-4; 
         }
         drag.style.left = x + 'px';
         left.style.width = x + 'px';
@@ -96,12 +85,8 @@
     }
     
     drag.addEventListener('mousedown', onDown, false); 
-    //add listener to actual drag div, if user clicks on it
-    //drag.addEventListener('touchstart', onDown);
     document.body.addEventListener('mouseup', onUp, false);
-    //document.body.addEventListener('mo', onUp);
     document.body.addEventListener('mousemove', onMove, false);
-    //document.body.addEventListener('touchmove', onMove);
 })();
 
   //3D viewer
@@ -118,4 +103,12 @@
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+
+  function scrollToSection(sectionId) {
+        gsap.to(window, { duration: 1, scrollTo: sectionId });
+    }
+
+
 })();
